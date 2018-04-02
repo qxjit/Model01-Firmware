ac99f4dae7955a9edd2318f8092fceec1b894f04
David Vollbracht <david.vollbracht@gmail.com>
2018-04-02 07:44:12 -0400

    Removes Function keys that duplicate Symobl keys

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -170,10 +170,10 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_LeftControl, Key_Delete, ___, ___,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10, Key_F11,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, ___,                      ___,                      ___,             ___,     Key_F12,
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,     ___,
+   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             ___,     ___,
    ___, ___, Key_Enter, ___,
    ___)
 
