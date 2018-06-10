98a0bc245dc2cd79e2d20ea1a71f9b018be4394c
David Vollbracht <david.vollbracht@gmail.com>
2018-06-10 07:30:09 -0400

    Removes OSM(LeftAlt) that I never use.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -111,7 +111,7 @@ KEYMAPS(
 
   [DVORAK] = KEYMAP_STACKED
   (___,              Key_1,          Key_2,      Key_3,        Key_4,    Key_5, Key_LEDEffectNext,
-   Key_Backtick,     Key_Quote,      Key_Comma,  Key_Period,   Key_P,    Key_Y, OSM(LeftAlt),
+   Key_Backtick,     Key_Quote,      Key_Comma,  Key_Period,   Key_P,    Key_Y, ___,
    Key_PageUp,       Key_A,          Key_O,      Key_E,        Key_U,    Key_I,
    Key_PageDown,     Key_Semicolon,  Key_Q,      Key_J,        Key_K,    Key_X, Key_Escape,
    OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
