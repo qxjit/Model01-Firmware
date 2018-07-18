b8e92abc4e5e8e832be3df250fe7ce9aa8fd1566
David Vollbracht <david.vollbracht@gmail.com>
2018-07-18 15:14:05 -0400

    Adds color modifier when shift is pressed

--- QxjitLED/QxjitLED.cpp
+++ QxjitLED/QxjitLED.cpp
@@ -1,4 +1,6 @@
 #include <QxjitLED.h>
+#include <Kaleidoscope-OneShot.h>
+#include <kaleidoscope/hid.h>
 
 namespace kaleidoscope {
 
@@ -6,25 +8,40 @@ QxjitLED::QxjitLED() {
 }
 
 void QxjitLED::update(void) {
-  cRGB color;
+  cRGB baseColor;
 
   uint8_t topLayer = Layer.top();
 
   switch (topLayer % 3) {
     case 2:
-      color = CRGB(0xdd, 0xdd, 0x00);
+      baseColor = CRGB(0xdd, 0xdd, 0x00);
       break;
 
     case 1:
-      color = CRGB(0xdd, 0x00, 0xdd);
+      baseColor = CRGB(0xdd, 0x00, 0xdd);
       break;
 
     default:
-      color = CRGB(0x00, 0xdd, 0xdd);
+      baseColor = CRGB(0x00, 0xdd, 0xdd);
+  }
+
+  cRGB modColor = baseColor;
+
+  if (hid::isModifierKeyActive(Key_LeftShift) ||
+      ::OneShot.isModifierActive(Key_LeftShift) ||
+      hid::isModifierKeyActive(Key_RightShift) ||
+      ::OneShot.isModifierActive(Key_RightShift)) {
+    modColor = CRGB(0xff, 0x00, 0x00);
   }
 
   for (uint8_t r = 0; r < ROWS; r++) {
     for (uint8_t c = 0; c < COLS; c++) {
+      cRGB color = baseColor;
+
+      if ((r % 2) == (c % 2)) {
+        color = modColor;
+      }
+
       ::LEDControl.setCrgbAt(r, c, color);
     }
   }
